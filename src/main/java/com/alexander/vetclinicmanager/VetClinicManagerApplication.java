d2e1e938f931d700b0d8c66445f83dde6082043d
Alex De <segoy@interia.pl>
2023-12-18 13:30:19 +0100

    first commit

---
+++ src/main/java/com/alexander/vetclinicmanager/VetClinicManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alexander.vetclinicmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetClinicManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexander/vetclinicmanager/VetClinicManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexander.vetclinicmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
