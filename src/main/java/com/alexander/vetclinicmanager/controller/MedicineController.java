9ce1eb67dd501a9b7701aae6906d0663b97cb2c4
Alex De <segoy@interia.pl>
2024-01-11 14:25:27 +0100

    add MedicineController

---
+++ src/main/java/com/alexander/vetclinicmanager/controller/MedicineController.java
@@ -0,0 +1,57 @@
+package com.alexander.vetclinicmanager.controller;
+
+import com.alexander.vetclinicmanager.model.Medicine;
+import com.alexander.vetclinicmanager.service.MedicineService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/medicine")
+public class MedicineController {
+
+    private final MedicineService medicineService;
+
+    public MedicineController(MedicineService medicineService) {
+        this.medicineService = medicineService;
+    }
+
+
+    @PostMapping("/newMedicine")
+    public Medicine createMedicine(@RequestBody Medicine medicine){
+        return medicineService.createMedicine(medicine);
+    }
+
+
+    @PatchMapping("/update/{id}")
+    public Medicine updateMedicine(@PathVariable Long id, @RequestBody Medicine medicine){
+        return medicineService.updateMedicine(id, medicine);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteMedicine(@PathVariable Long id){
+        medicineService.deleteMedicine(id);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/alexander/vetclinicmanager/service/MedicineService.java
+++ src/main/java/com/alexander/vetclinicmanager/service/MedicineService.java
@@ -1,4 +1,62 @@
 package com.alexander.vetclinicmanager.service;
 
+import com.alexander.vetclinicmanager.model.Medicine;
+import com.alexander.vetclinicmanager.repository.MedicineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
 public class MedicineService {
+
+    @Autowired
+    MedicineRepository medicineRepository;
+
+
+    public Medicine createMedicine(Medicine medicine){
+        return medicineRepository.save(medicine);
+    }
+
+
+    @Transactional
+    public Medicine updateMedicine(Long id, Medicine medicine){
+        Medicine existingMedicine = medicineRepository.findById(id).
+                orElseThrow(() -> new IllegalArgumentException("Medicine not found"));
+
+        existingMedicine.setName(medicine.getName());
+        existingMedicine.setPrice(medicine.getPrice());
+        existingMedicine.setStock(medicine.getStock());
+
+        return medicineRepository.save(existingMedicine);
+    }
+
+
+    public void deleteMedicine(Long id){
+        Medicine existingMedicine = medicineRepository.findById(id).
+                orElseThrow(() -> new IllegalArgumentException("Medicine not found"));
+
+        medicineRepository.delete(existingMedicine);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
